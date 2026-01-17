42dc66248de9ca5247054628794ed0bf083a33fd
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-01-18 00:49:14 +0100

    Version Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "fr.amazonia"
-version = "0.0.2-SNAPSHOT" //2 Version
+version = "0.0.3-SNAPSHOT" //3 Version
 description = "DestriumMod"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
